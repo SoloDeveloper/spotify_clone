cbb8b1d747ba6ef31d938ee6cbce3a5aff2c9e85
Jonathan Stafford <solomystery1@gmail.com>
2023-07-30 18:55:13 -0500

    Foundation of the sidebar started

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import SideBar from '@/components/Sidebar';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
@@ -9,14 +10,14 @@ export const metadata: Metadata = {
 	description: 'Listen to music!',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
+	return (
+		<html lang="en">
+			<body className={font.className}>
+				<SideBar>
+					{children}
+				</SideBar>
+			</body>
+		</html>
+	);
 }
