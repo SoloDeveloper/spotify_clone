2dd5af0ab87c26ca1281549860f1704d4a04af61
Jonathan Stafford <solomystery1@gmail.com>
2023-07-30 21:30:20 -0500

    Created UserProvider and Context
    This will be useful for quickly being able to grab the users data and knowing if theyre a paying subscriber

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import SideBar from '@/components/Sidebar';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import UserProvider from '@/providers/UserProvider';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -14,9 +16,13 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
 	return (
 		<html lang="en">
 			<body className={font.className}>
-				<SideBar>
-					{children}
-				</SideBar>
+				<SupabaseProvider>
+					<UserProvider>
+						<SideBar>
+							{children}
+						</SideBar>
+					</UserProvider>
+				</SupabaseProvider>
 			</body>
 		</html>
 	);
