7b563253520a2f58496aba202f2acc342aeb53bf
Jonathan Stafford <solomystery1@gmail.com>
2023-07-31 11:57:32 -0500

    Added the ability to like songs

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 
@@ -35,7 +36,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
 							data={song} 
 						/>
 					</div>
-					{/* TODO: Add Like Button Here */}
+					<LikeButton songId={song.id}/>
 				</div>
 			))}
 		</div>
